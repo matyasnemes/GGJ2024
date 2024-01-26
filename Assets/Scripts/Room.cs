3edb9d83138cf8cd37b3378990d0a86a6287c8ba
Peter Gyulai <peterskhan1997@gmail.com>
2024-01-26 21:22:04 +0100

    Rooms collect their workstations

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public List<Character> charactersInRoom;
+    public List<WorkStation> workStationsInRoom;
+
+    public void EnterRoom(Character character)
+    {
+        charactersInRoom.Add(character);
+    }
+
+    public void LeaveRoom(Character character)
+    {
+        charactersInRoom.Remove(character);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        workStationsInRoom = new List<WorkStation>(GetComponentsInChildren<WorkStation>());
     }
 
     // Update is called once per frame
