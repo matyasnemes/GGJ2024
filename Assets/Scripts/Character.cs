aca4e382464addad2931ba43121550adea971ad5
kooofic <51694706+kooofic@users.noreply.github.com>
2024-01-26 22:11:39 +0100

    Update Character.cs

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    const float eps = 0.0001;
+
+    enum State
+    {
+        Going,
+        Working,
+        DoneWorking,
+    }
+
+    enum Target
+    {
+        Door,
+        Workstation
+    }
+
+    State state;
+
+    public Vector2 goal = null;
+
+    Target currentTarget;
+    Room currentRoom;
+    Room targetRoom;
+    WorkStation targetWorkstation;
 
     /**
      * Requests the character to leave its current workstation.
@@ -11,18 +34,51 @@ public class Character : MonoBehaviour
      */
     public void RequestLeaveWorkstation(WorkStation workStation)
     {
-
+        if(state == Working)
+        {
+            state = DoneWorking;
+        } 
+        else
+        {
+            Debug.Log("<color=red>Error: </color> Invalid state change to DoneWorking");
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //TODO: Generate random workstation as target
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch(state)
+        {
+            case Going:
+                /*
+                *   Go
+                *   If reached targed, do the thing
+                *   Room registration
+                */
+            break;
+            case Working:
+
+                //Work, work
+
+            break;
+            case DoneWorking:
+            {
+                /*
+                * Deregister from workstation
+                * Find new goal
+                * Set state
+                */
+            }
+            default:
+                //What
+            break
+        }
         
     }
 }
