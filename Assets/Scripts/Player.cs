6b6837266d0b749ef5c471e436fe21299da67e47
Peter Gyulai <peterskhan1997@gmail.com>
2024-01-27 14:21:27 +0100

    Player controlling with collision and room detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,62 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+    private CharacterDisplay characterDisplay;
+    private Room currentRoom = null;
+
+    /**
+     * Tells the specified joke type in the current room of the player.
+     * @param[in] The type of the joke to tell.
+     */
+    public void TellJokeInCurrentRoom(JokeItemType jokeType)
+    {
+        if (currentRoom)
+        {
+            currentRoom.TellJoke(jokeType);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        characterDisplay = GetComponentInChildren<CharacterDisplay>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Room room = collider.gameObject.GetComponent<Room>();
+        if (room)
+        {
+            Debug.Log("Player entered room: " + room.name);
+            currentRoom = room;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        Room room = collider.gameObject.GetComponent<Room>();
+        if (room)
+        {
+            Debug.Log("Player left room: " + room.name);
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 velocity = Vector2.zero;
+        characterDisplay.FaceDown();
         float speed = 10.0f;
 
         if (Input.GetKey(KeyCode.W)) 
         { 
             velocity += Vector2.up * speed;
+            characterDisplay.FaceUp();
         }
         else if (Input.GetKey(KeyCode.S)) 
         { 
             velocity += Vector2.down * speed;
+            characterDisplay.FaceDown();
         }
 
         if (Input.GetKey(KeyCode.A)) 
