97f9f5d54fdb1b89d56daee4ce76eb455896d247
nemes.matyas <nemes.matyas@fornax.hu>
2024-01-27 13:07:05 +0100

    NPCs
    NPCs

---
+++ Assets/Scripts/CharacterDisplay.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDisplay : MonoBehaviour
+{
+
+    public Sprite headDown;
+    public Sprite headUp;
+    public Sprite bodyDown;
+    public Sprite bodyUp;
+    public Sprite hand;
+
+    public SpriteRenderer headRenderer;
+    public SpriteRenderer bodyRenderer;
+    public SpriteRenderer handRenderer1;
+    public SpriteRenderer handRenderer2;
+
+    public bool faceDown = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(faceDown) {
+            FaceDown();
+        }
+        else {
+            FaceUp();
+        }
+        handRenderer1.sprite = hand;
+        handRenderer2.sprite = hand;
+    }
+
+    public void FaceDown() {
+        headRenderer.sprite = headDown;
+        bodyRenderer.sprite = bodyDown;
+        headRenderer.sortingOrder = 11;
+        handRenderer1.sortingOrder = 11;
+        handRenderer2.sortingOrder = 11;
+        bodyRenderer.sortingOrder = 10;
+    }
+
+    public void FaceUp() {
+        headRenderer.sprite = headUp;
+        bodyRenderer.sprite = bodyUp;
+        headRenderer.sortingOrder = 10;
+        handRenderer1.sortingOrder = 10;
+        handRenderer2.sortingOrder = 10;
+        bodyRenderer.sortingOrder = 11;
+    }
+}
