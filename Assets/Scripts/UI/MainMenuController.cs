811ec8c83431da02049340c0de491d98f9d1c794
nemes.matyas <nemes.matyas@fornax.hu>
2024-01-28 18:37:03 +0100

    Main Menu
    Main Menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+
+    public GameObject helpScreen;
+
+    private bool helpVisible = false;
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
@@ -13,5 +17,19 @@ public class MainMenuController : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void Help()
+    {
+        if(helpVisible)
+        {
+            helpScreen.SetActive(false);
+            helpVisible = false;
+        }
+        else
+        {
+            helpScreen.SetActive(true);
+            helpVisible = true;
+        }
     }
 }
