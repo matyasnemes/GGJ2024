6815bd5f33ccd2a795528bc424b10d2285b67351
jszorfi <44711471+jszorfi@users.noreply.github.com>
2024-01-28 13:48:32 +0100

    Fix robot laughing UI

--- Assets/Scripts/UI/RobotJokes.cs
+++ Assets/Scripts/UI/RobotJokes.cs
@@ -3,16 +3,36 @@ using UnityEngine.UI;
 
 public class RobotJokes : MonoBehaviour
 {
+    public Sprite laugh;
+    public Sprite meh;
+
     void Start()
     {
         var gameController = GameObject.Find("GameController").GetComponent<GameController>();
         var jokes = gameController.GetRobotFunnyJokes();
 
-        for (int i = 0; i < jokes.Count; i++)
+        var pun = transform.GetChild(1).gameObject.GetComponent<Image>();
+        var mom = transform.GetChild(2).gameObject.GetComponent<Image>();
+        var dark = transform.GetChild(3).gameObject.GetComponent<Image>();
+
+        pun.sprite = meh;
+        mom.sprite = meh;
+        dark.sprite = meh;
+
+        foreach (var joke in jokes)
         {
-            var image = transform.GetChild(i + 1).gameObject.GetComponent<Image>();
-            image.sprite = jokes[i].sprite();
-            image.enabled = true;
+            if (joke.name() == "Dark Joke")
+            {
+                dark.sprite = laugh;
+            }
+            else if (joke.name() == "Mom Joke")
+            {
+                mom.sprite = laugh;
+            }
+            else if (joke.name() == "Pun")
+            {
+                pun.sprite = laugh;
+            }
         }
     }
 }
