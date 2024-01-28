8a53c758789a7945a7d2e9c0bb66df41205f9674
jszorfi <44711471+jszorfi@users.noreply.github.com>
2024-01-28 16:10:38 +0100

    Camera fixes again

--- Assets/Scripts/MapCameraController.cs
+++ Assets/Scripts/MapCameraController.cs
@@ -2,37 +2,70 @@ using UnityEngine;
 
 public class MapCameraController : MonoBehaviour
 {
-    private float minCameraSize = 15f;
-    private float maxCameraSize = 44f;
-    private float camereScrollSensitivity = 5f;
+    public Transform finalPlace;
+
+    private float minSize = 20f;
+    private float maxSize = 44f;
+    private float starterSize;
+    private float dragSpeed = 10f;
+    private float zoomOutSpeed;
+    private float zoomInSpeed;
 
     private Camera mapCamera;
     private bool isZoomingOut = false;
+    private bool isZoomingIn = false;
+    private bool isMoving = false;
+
     private void Start()
     {
         mapCamera = GetComponent<Camera>();
+        var fin = finalPlace.position;
+        fin.z = transform.position.z;
+        finalPlace.position = fin;
     }
 
     public void ZoomOut()
     {
         isZoomingOut = true;
+        isMoving = true;
+        starterSize = mapCamera.orthographicSize;
+
+        var allDistance = Vector3.Distance(transform.position, finalPlace.position);
+        var allTime = allDistance / dragSpeed;
+        zoomOutSpeed = (maxSize - starterSize) / (allTime / 2.0f);
+        zoomInSpeed = (maxSize - starterSize) / (allTime / 2.0f);
     }
-    public void ZoomOut(float zoom)
+    void ZoomOut(float zoom)
     {
         var size = mapCamera.orthographicSize;
-        size += zoom * camereScrollSensitivity;
-        if (size > maxCameraSize)
+        size += zoom * zoomOutSpeed;
+        if (size > maxSize)
         {
-            size = maxCameraSize;
+            size = maxSize;
             isZoomingOut = false;
+            isZoomingIn = true;
         }
 
         mapCamera.orthographicSize = size;
     }
 
-    public void ZoomTo()
+    void ZoomIn(float zoom)
     {
+        var size = mapCamera.orthographicSize;
+        size -= zoom * zoomInSpeed;
+        if (size < minSize)
+        {
+            size = minSize;
+            isZoomingIn = false;
+        }
 
+        mapCamera.orthographicSize = size;
+    }
+
+    void MoveTo()
+    {
+        Vector3 newpos = Vector3.MoveTowards(transform.position, finalPlace.position, dragSpeed * Time.deltaTime);
+        transform.position = newpos;
     }
 
     void Update()
@@ -40,6 +73,14 @@ public class MapCameraController : MonoBehaviour
         if (isZoomingOut)
         {
             ZoomOut(Time.deltaTime);
+        }
+        else if (isZoomingIn)
+        {
+            ZoomIn(Time.deltaTime);
+        }
+        if (isMoving)
+        {
+            MoveTo();
         }
     }
 }
